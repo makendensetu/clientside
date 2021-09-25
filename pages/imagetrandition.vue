48a28f0c953339d4404b9e631d7672c8a7b0916a
Haruten <47566372+Haruten@users.noreply.github.com>
2021-09-26 04:25:50 +0900

    imagetrandtion's CSS

--- pages/imagetrandition.vue
+++ pages/imagetrandition.vue
@@ -1,7 +1,24 @@
 <template>
-  <imagesender
-    class="mx-auto max-w-screen-md max-h-64 p-6 z-10 relative frop-shadow"
-  />
+  <div>
+    <div>
+      <imagesender
+        class="
+          top-1/3
+          left-1/2
+          transform
+          -translate-x-1/2 -translate-y-1/2
+          bg-yellow-100
+          border border-slid border-black
+          rounded-lg
+          max-w-screen-md max-h-84
+          p-6
+          z-10
+          fixed
+          frop-shadow
+        "
+      />
+    </div>
+  </div>
 </template>
 
 <script>
