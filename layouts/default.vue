9cb7375e4889903ff143ae975ec9e7e975330a62
inaridiy <inainainariinainari@gmail.com>
2021-09-26 02:16:26 +0900

    fsdfsdf

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="relative cut h-screen">
+    <figure class="absolute w-full h-full z-0">
+      <nuxt-img
+        src="/image/inou_top.png"
+        class="w-full h-full object-cover"
+      ></nuxt-img>
+      <figcaption class="absolute bottom-0 bg-black text-white p-1">
+        背景の出典：<cite
+          >古地図コレクション https://kochizu.gsi.go.jp/inouzu</cite
+        >
+      </figcaption>
+    </figure>
+
+    <nuxt />
+  </div>
+</template>
+<script>
+export default {
+  mounted() {
+    console.log('inariS')
+  },
+}
+</script>
--- pages/imagetrandition.vue
+++ pages/imagetrandition.vue
@@ -1,22 +1,9 @@
 <template>
- <figure>
-    <nuxt-img
-        src="/image/inou_top.png"
-        class="fixed w-full h-full object-cover"
-      ></nuxt-img>
-      <imagesender class="mx-auto max-w-screen-md max-h-64 p-6 z-10 relative frop-shadow"/>
-    <div class="relative cut h-screen">
-      <figcaption class="absolute bottom-0 bg-black text-white p-1">
-        背景の出典：<cite
-          >古地図コレクション https://kochizu.gsi.go.jp/inouzu</cite
-        >
-      </figcaption>
-    </div>
- </figure>
+  <imagesender
+    class="mx-auto max-w-screen-md max-h-64 p-6 z-10 relative frop-shadow"
+  />
 </template>
 
 <script>
-    export default {
-  
-}
+export default {}
 </script>
