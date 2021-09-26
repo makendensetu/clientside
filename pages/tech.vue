d6aa62ae6635fef971d8ba50e16c226c76e60ae4
Haruten <47566372+Haruten@users.noreply.github.com>
2021-09-26 09:36:00 +0900

    tech.vueの追加

--- pages/tech.vue
+++ pages/tech.vue
@@ -1,3 +1,23 @@
 <template>
-  <div></div>
+  <div>
+    <h1 class="text-5xl font-semibold text-center py-3">仕様技術</h1>
+    <ul class="text-center">
+      <li><a href="https://nuxtjs.org/">Nuxt</a></li>
+      <li><a href="https://sharp.pixelplumbing.com/">sharp</a></li>
+      <li><a href="https://expressjs.com/ja/">express</a></li>
+      <li><a href="https://pptr.dev/">puppeteer</a></li>
+      <li><a href="https://pptr.dev/">mapbox</a></li>
+    </ul>
+    <div class="flex justify-center">
+      <BtnTypeA class="mt-16" />
+    </div>
+  </div>
 </template>
+<style>
+ul {
+  font-size: 54px;
+  font-family: 'Courier New', Courier, monospace;
+  font-style: normal;
+  font-weight: bolder;
+}
+</style>
